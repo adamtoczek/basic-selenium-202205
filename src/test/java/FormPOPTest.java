82dff4db2451030502bcdd2ac6625f3d41a625d9
Adam Toczek <a.a.toczek@gmail.com>
2022-05-21 16:30:47 +0200

    selenium POP cwiczenia na formularzu

--- src/test/java/FormPOPTest.java
+++ src/test/java/FormPOPTest.java
@@ -1,5 +1,9 @@
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.FormPage;
+import pages.Sex;
+
+import java.util.Arrays;
 
 public class FormPOPTest extends TestBase{
 
@@ -11,6 +15,17 @@ public class FormPOPTest extends TestBase{
         formPage.selectRandomProfession();
         formPage.selectContinent("North America");
         formPage.setFirstName("Adam");
+        formPage.setLastName("Kwiatkowski");
+        formPage.setAge("26");
+        formPage.setEmail("adam@test.pl");
+        formPage.setSex(Sex.MALE);
+        formPage.setFile("C:\\Test\\test.txt");
+        formPage.setExperience(7);
+        formPage.setSeleniumCommands(Arrays.asList(new String[]{"Browser Commands", "Wait Commands", "Navigation Commands"}));
+
+        formPage.submitForm();
+
+        Assert.assertEquals(formPage.getValidatorMessage(), "Form send with success");
 
         Thread.sleep(5000);
     }
